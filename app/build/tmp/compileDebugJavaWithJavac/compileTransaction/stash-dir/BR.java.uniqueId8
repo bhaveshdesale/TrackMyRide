b05c400b3fbf3de84c14847abb423a75908eb0d0
bhaveshdesale <bhaveshdesale16@gmail.com>
2024-09-03 00:06:36 +0530

    This is my second Commit

---
+++ app/build/tmp/compileDebugJavaWithJavac/compileTransaction/stash-dir/BR.java.uniqueId8
@@ -0,0 +1,5 @@
+package com.example.trackmyride;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/src/main/java/com/example/trackmyride/ui/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.example.trackmyride.ui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.trackmyride.R;
+import com.example.trackmyride.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Use ViewBinding to inflate the layout
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Enable Edge to Edge UI
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set the background of the BottomNavigationView to null
+        binding.bottomNavigationView.setBackground(null);
+
+        // Replace the fragment with HomeFragment by default
+        replaceFragment(new HomeFragment());
+
+        // Handle BottomNavigationView item selection
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+//                case R.id.home:
+//                    replaceFragment(new HomeFragment());
+//                    break;
+//                case R.id.profile:
+//                    replaceFragment(new ProfileFragement()); // Corrected the name here
+//                    break;
+            }
+            return true;
+        });
+    }
+
+    // Method to replace the current fragment
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
